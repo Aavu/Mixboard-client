fc16abacaca40522b3515d4e73fb6d23294149fd
aavu <aavu@gatech.edu>
2022-10-13 08:43:24 -0400

    Initial Commit

---
+++ Mashup/MashupApp.swift
@@ -0,0 +1,17 @@
+//
+//  MashupApp.swift
+//  Mashup
+//
+//  Created by Raghavasimhan Sankaranarayanan on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MashupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
